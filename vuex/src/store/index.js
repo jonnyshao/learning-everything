64d64c565c2438609bc42c27c8abe59a52bf0bca
jonnyshao <443547559@qq.com>
2019-03-23 17:55:53 +0800

    add mvvm demo

--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -7,6 +7,7 @@ import getters from './getters'
 import user from './modules/user'
 Vue.use(Vuex)
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
   state,
   actions,
   getters,
