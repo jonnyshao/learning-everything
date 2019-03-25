990b5bddbfb337aabbbb121c6ef7286a2ecf5c52
jonnyshao <443547559@qq.com>
2019-03-25 21:52:46 +0800

    add pwa demo

--- jwt-vue/src/main.js
+++ jwt-vue/src/main.js
@@ -7,9 +7,40 @@ import 'iview/dist/styles/iview.css';
 
 Vue.use(iView);
 Vue.config.productionTip = false
-
+import Test from './components/Test'
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => {
+    return h(Test,{
+      props:{
+        msg: 'Jonny'
+      },
+      nativeOn: {
+        click(){
+          alert(1)
+        }
+      }
+    },'内容')
+    // return <h1
+    // on-click={() => alert(1)}
+    // style={{color: 'red'}}>你好 </h1>
+    // return h('h1',{ // jsx语法
+    //   attrs: {
+    //     id: 1
+    //   },
+    //   style: {
+    //     color: 'red'
+    //   },
+    //   class: ['a','b'],
+    //   on: {
+    //     click(){
+    //       alert(1)
+    //     }
+    //   },
+    //   domProps: {
+    //     innerHTML: 'Hello'
+    //   }
+    // },'内容')
+  }
 }).$mount('#app')
