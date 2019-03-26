a0c6d756f5a7b608f65d74fa40bccaebc9001bec
jonnyshao <443547559@qq.com>
2019-03-26 19:11:37 +0800

    add vue-ssr

---
+++ vue-ssr/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <Bar></Bar>
+    <Foo></Foo>
+  </div>
+</template>
+<script>
+import Foo from './components/Foo'
+import Bar from './components/Bar'
+export default {
+  name: 'App',
+  components: {
+    Foo,
+    Bar
+  }
+}
+</script>
---
+++ vue-ssr/src/components/Bar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bar" @click="show">
+    Bar COMPONENTS
+  </div>
+</template>
+<script>
+export default {
+  name: 'Bar',
+  methods: {
+    show () {
+
+    }
+  }
+}
+</script>
+<style>
+.bar{
+  color: darkblue
+}
+</style>
